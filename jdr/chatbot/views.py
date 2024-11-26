8400c2cb86d05f992b8c335a82cd8ab304d4ef37
Alex <alexandre.caraes@isen-ouest.yncrea.fr>
2024-11-27 00:43:12 +0100

    finito le chat

--- jdr/chatbot/views.py
+++ jdr/chatbot/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .openaicall import response
+from .models import Conversation
 
 @login_required(login_url="login")
 def chat(request):
@@ -8,4 +9,7 @@ def chat(request):
     if request.method == "POST":
         prompt = request.POST.get("prompt")
         reponse = response(prompt)
-    return render(request, 'chat.html', {'reponse': reponse})
+        Conversation.objects.create(user=request.user, prompt=prompt, response=reponse)
+        
+    conversations = Conversation.objects.filter(user=request.user).order_by('-timestamp')
+    return render(request, 'chat.html', {'reponse': reponse, 'conversations': conversations})
