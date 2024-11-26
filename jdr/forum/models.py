0801f86f0efc0d950fa3278c243ed7dd125aef0d
Alex <alexandre.caraes@isen-ouest.yncrea.fr>
2024-11-26 11:21:09 +0100

    fin du forum

--- jdr/forum/models.py
+++ jdr/forum/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Article(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-class Comment(models.Model):
-    article = models.ForeignKey(Article, related_name='comments', on_delete=models.CASCADE)
-    content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-
 class Thread(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
